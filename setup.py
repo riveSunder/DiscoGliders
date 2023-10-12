0342872890e9e50e921e604354a4fb987ea5e575
RiveSunder <rivesunder@protonmail.com>
2023-10-11 19:24:48 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(\
+        name="DiscoGliders", \
+        packages=["disco"], \
+        version = "0.0", \
+        description = "Investigating discretization effects on gliders in (Dis)Continuous Cellular Automata", \
+    )
+
+
